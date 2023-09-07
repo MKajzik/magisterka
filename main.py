b86b129196fdb08d7e950510da97a10f0e8182bd
UKE12394441 <mateusz.kajzik@tesco.com>
2023-09-07 21:36:45 +0200

    Added water function

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ spi.max_speed_hz=1000000
 soil_pin = 15
 temp_pin = 6
 delay = 10
+water_pump_pin = 9
 
 if (os.path.isfile("./humidity_temperature.json") == True):
 	f = open('humidity_temperature.json')
@@ -72,6 +73,7 @@ wiringpi.wiringPiSetup()
 wiringpi.digitalWrite(16, GPIO.HIGH)
 wiringpi.pinMode(soil_pin, GPIO.OUTPUT)
 wiringpi.pinMode(16, GPIO.OUTPUT)
+wiringpi.pinMode(water_pump_pin, GPIO.OUTPUT)
 
 class DateTimeEncoder(json.JSONEncoder):
         #Override the default method
@@ -153,6 +155,38 @@ def TempGetResult(pin):
 		wiringpi.delay(200)
 	return SH,SL,TH,TL,flag
 
+def Humidity(pin):
+	airVoltage = 2.5
+	waterVoltage = 1.25
+	half = (airVoltage - waterVoltage)/3
+
+	wiringpi.digitalWrite(pin, GPIO.LOW)
+	wiringpi.delay(100)
+	value = ReadChannel3208(0)
+	wiringpi.delay(100)
+	wiringpi.digitalWrite(pin, GPIO.HIGH)
+	voltage = ConvertToVoltage(value, 12, 3.3)
+
+	if(voltage > waterVoltage and voltage < (waterVoltage + half)):
+		return False
+	elif(voltage < airVoltage and voltage > (airVoltage - half)):
+		return True
+
+def WaterPlant(pin):
+	now = datetime.datetime.now()
+	startTime = now.replace(hour=20, minute=0, second=0, microsecond=0)
+	endTime = now.replace(hour=23, minute=0, second=0, microsecond=0)
+	if(now > startTime and now < endTime):
+		print("Starting to water plant")
+		wateringStatus = Humidity(pin)
+		while wateringStatus:
+			wiringpi.digitalWrite(water_pump_pin, GPIO.HIGH)
+			wiringpi.delay(5000) # 5 seconds
+			wiringpi.digitalWrite(water_pump_pin, GPIO.LOW)
+			wiringpi.delay(10000) # 1200000 = 20mins
+			wateringStatus = Humidity(pin)
+
+
 def PrintHumi(pin):
 	airVoltage = 2.5
 	waterVoltage = 1.25
@@ -167,12 +201,13 @@ def PrintHumi(pin):
 	ct = datetime.datetime.now()
 	print(ct,"-",f"{voltage:.3f}","V")
 
-	if(voltage > waterVoltage and value < (waterVoltage + intervals)):
+	if(voltage > waterVoltage and voltage < (waterVoltage + intervals)):
 		print("Bardzo mokro")
 	elif(voltage > (waterVoltage + intervals) and voltage < (airVoltage - intervals)):
 		print("Mokro")
 	elif(voltage < airVoltage and voltage > (airVoltage - intervals)):
 		print("Sucho")
+		WaterPlant(pin)
 
 	soil_moisture["soil_moisture_actual"]["value"] = voltage
 	soil_moisture["soil_moisture_actual"]["timestamp"] = ct
